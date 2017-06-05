c9d3e26ca3bc9f302016847913b33e31a332a896
xq.tian <springqiang@gmail.com>
2017-06-05 11:57:58 +0800

    add select author in some time

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message && message.msg == "initSel") {
+        initSel();
+    }
+});
+
 function initSel() {
     var targetSel = $("#issue_assigned_to_id");
     var memberSel = $("#issue_custom_field_values_4");
@@ -9,9 +15,34 @@ function initSel() {
     memberSel.next().width('95%');
 }
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if(message && message.msg == "initSel"){
-        initSel();
+function selectAuthor() {
+    var author = $("p.author>a.user.active")[0].innerText;
+    $("#issue_assigned_to_id option").each(function () {
+        var t = $(this);
+        if(t.text() == author) {
+            var targetSel = $("#issue_assigned_to_id");
+            targetSel.val(t.val());
+            return;
+        }
+    });
+}
+
+function initEvent() {
+    var st = $("#issue_status_id");
+    // 驳回或待验证
+    if (st.val() == 6 || st.val() == 24) {
+        selectAuthor();
+    }
+    setTimeout(function () {
+        $("#issue_status_id").change(initEvent);
+    }, 700);
+}
+
+$(function () {
+    var issue = $("#issue_tracker_id");
+    // 缺陷类型
+    if (issue.val() == 6) {
+        $("#issue_status_id").change(initEvent);
     }
 });
 
